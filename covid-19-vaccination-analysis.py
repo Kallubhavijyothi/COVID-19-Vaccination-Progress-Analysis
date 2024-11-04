e1153c52562440567eae3aeb587fb8ec00cec815
Kallubhavijyothi <kallubhavijyothi@gmail.com>
2024-11-04 14:16:16 +0300

    add dataset import

--- covid-19-vaccination-analysis.py
+++ covid-19-vaccination-analysis.py
@@ -3,3 +3,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from datetime import datetime
+
+# Load the dataset
+url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv'
+df = pd.read_csv(url)
